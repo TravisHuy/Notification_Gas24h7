2513914d6781c31e06d95b8f3a7b07b4b647910a
TravisHuy <honhathuy098@gmail.com>
2024-10-21 12:09:30 +0700

    first commit

---
+++ src/test/java/org/nhathuy/notification_gas24h7/NotificationGas24h7ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nhathuy.notification_gas24h7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationGas24h7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
